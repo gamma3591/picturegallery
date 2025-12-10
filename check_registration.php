d95efedc8e8d1497fa2553626bdb04698b354cb0
Mathilde Paya <mathilde.paya@student-cs.fr>
2025-12-10 09:55:22 +0100

    fixing sql injection issues

--- check_registration.php
+++ check_registration.php
@@ -8,34 +8,41 @@ include 'includes/header.html';
 
 include 'includes/navbar.html';
 
-if (isset ($_SESSION['username'])){
-	header('location: index.php');
-}
-else{
-	
+if (isset($_SESSION['username'])) {
+    header('location: index.php');
+} else {
+    
     $username = $_POST['username'];
     $password = $_POST['pass'];
 
+    $stmt1 = mysqli_prepare($connection,
+        "SELECT users_username, users_password 
+         FROM users 
+         WHERE users_username = ? AND users_password = ?"
+    );
+    mysqli_stmt_bind_param($stmt1, "ss", $username, $password);
+    mysqli_stmt_execute($stmt1);
+    $result1 = mysqli_stmt_get_result($stmt1);
 
-    $sql1 = "SELECT users_username, users_password FROM users WHERE users_username = '{$username}' AND users_password = '{$password}'";
-    $sql2 = "INSERT INTO users (users_username, users_password) VALUES ('$username', '$password')";
+    if (mysqli_num_rows($result1) == 0) {
 
-    $result1 = mysqli_query ($connection, $sql1) or die (mysqli_error ($connection));
+        $stmt2 = mysqli_prepare($connection,
+            "INSERT INTO users (users_username, users_password) 
+             VALUES (?, ?)"
+        );
+        mysqli_stmt_bind_param($stmt2, "ss", $username, $password);
 
-    if (mysqli_num_rows ($result1) == 0){
-        if (mysqli_query ($connection, $sql2)){
+        if (mysqli_stmt_execute($stmt2)) {
             include 'includes/new_registration.php';
-        }
-        else{
+        } else {
             include 'includes/error.php';
-            
         }
 
-    }
-    else{
+    } else {
         include 'includes/notregistered.php';
     }
 }
+
 
 mysqli_close ($connection);
 
